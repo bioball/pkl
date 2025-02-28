b2b4bdeb3f6430f941cf76f5be6e54a031ba0674
Dan Chao <dan.chao@apple.com>
2025-02-28 15:11:47 -0800

    Fixup

--- bench/src/jmh/java/org/pkl/core/Withers.java
+++ bench/src/jmh/java/org/pkl/core/Withers.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.pkl.core;
 
 import java.util.concurrent.TimeUnit;
@@ -15,14 +30,15 @@ import org.openjdk.jmh.annotations.Warmup;
 @Fork(1)
 public class Withers {
   interface Wither<R, M> {
-    R with(Consumer<M> consumer);
+    R with(Consumer<M> setter);
   }
 
-  record Thing(String p1, String p2, String p3, String p4, String p5, String p6, String p7) implements Wither<Thing, Thing.Memento> {
+  record Thing(String p1, String p2, String p3, String p4, String p5, String p6, String p7)
+      implements Wither<Thing, Thing.Memento> {
     @Override
-    public Thing with(Consumer<Memento> consumer) {
+    public Thing with(Consumer<Memento> setter) {
       var mementor = new Memento(this);
-      consumer.accept(mementor);
+      setter.accept(mementor);
       return mementor.build();
     }
 
@@ -34,6 +50,7 @@ public class Withers {
       String p5;
       String p6;
       String p7;
+
       private Memento(Thing thing) {
         this.p1 = thing.p1;
         this.p2 = thing.p2;
@@ -49,17 +66,17 @@ public class Withers {
       }
     }
   }
-  
+
   @SuppressWarnings("UnusedReturnValue")
   record Thing2(String p1, String p2, String p3, String p4, String p5, String p6, String p7) {
     public Thing2 withP1(String p1) {
       return new Thing2(p1, p2, p3, p4, p5, p6, p7);
     }
-    
+
     public Thing2 withP2(String p2) {
       return new Thing2(p1, p2, p3, p4, p5, p6, p7);
     }
-    
+
     public Thing2 withP3(String p3) {
       return new Thing2(p1, p2, p3, p4, p5, p6, p7);
     }
@@ -84,27 +101,21 @@ public class Withers {
   @Benchmark
   public void withers() {
     var thing = new Thing("one", "two", "three", "four", "five", "six", "seven");
-    thing.with((m) -> {
-      m.p1 = "a";
-      m.p2 = "a";
-      m.p3 = "a";
-      m.p4 = "a";
-      m.p5 = "a";
-      m.p6 = "a";
-      m.p7 = "a";
-    });
+    thing.with(
+        (m) -> {
+          m.p1 = "a";
+          m.p2 = "b";
+          m.p3 = "c";
+          m.p4 = "d";
+          m.p5 = "e";
+          m.p6 = "f";
+          m.p7 = "g";
+        });
   }
 
   @Benchmark
   public void classicalWithers() {
     var thing = new Thing2("one", "two", "three", "four", "five", "six", "seven");
-    thing
-      .withP1("a")
-      .withP2("b")
-      .withP3("c")
-      .withP4("d")
-      .withP5("e")
-      .withP6("f")
-      .withP7("g");
+    thing.withP1("a").withP2("b").withP3("c").withP4("d").withP5("e").withP6("f").withP7("g");
   }
 }
